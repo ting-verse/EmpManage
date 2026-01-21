1a1e61361f3786da2797c3b28e5cf719dfb98343
chenting <3213043406@qq.com>
2026-01-21 10:19:32 +0800

    用户新增实现

--- manager-pei/src/views/User.vue
+++ manager-pei/src/views/User.vue
@@ -24,7 +24,7 @@
     </div>
     <div class="base-table">
       <div class="action">
-        <el-button type="primary">新增用户</el-button>
+        <el-button type="primary" @click="handleCreate">新增用户</el-button>
         <el-button type="danger" @click="handlePatchDelete">批量删除用户</el-button>
       </div>
       <el-table :data="userList" style="width: 100%" @selection-change="handleSelectionChange">
@@ -42,24 +42,69 @@
           </template>
         </el-table-column>
       </el-table>
-      <el-pagination layout="prev, pager, next" :total="pager.total"  @current-change="handleCurrentChange" class="pagination"/>
+      <el-pagination layout="prev, pager, next" :total="pager.total" @current-change="handleCurrentChange"
+        class="pagination" />
     </div>
+    <el-dialog title="新增用户" v-model="showModel">
+      <el-form :model="userForm" :rules="rules" ref="dialogForm" label-width="100px">
+        <el-form-item label="用户名" prop="userName">
+          <el-input v-model="userForm.userName" placeholder="请输入用户名" />
+        </el-form-item>
+        <el-form-item label="用户邮箱" prop="userEmail">
+          <el-input v-model="userForm.userEmail" placeholder="请输入用户邮箱">
+            <template #append>
+              @gmail.com
+            </template>
+          </el-input>
+        </el-form-item>
+        <el-form-item label="手机号" prop="mobile">
+          <el-input v-model="userForm.mobile" placeholder="请输入手机号" />
+        </el-form-item>
+        <el-form-item label="岗位" prop="job">
+          <el-input v-model="userForm.job" placeholder="请输入岗位" />
+        </el-form-item>
+        <el-form-item label="状态" prop="state">
+          <el-select v-model="userForm.state">
+            <el-option :value="1" label="在职"></el-option>
+            <el-option :value="2" label="离职"></el-option>
+            <el-option :value="3" label="试用期"></el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item label="系统角色" prop="roleList" placeholder="请选择系统角色">
+          <el-select v-model="userForm.roleList">
+            <el-option></el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item label="部门" prop="deptId">
+          <el-cascader v-model="userForm.deptId" :options="[]"
+            :props="{ checkStrictly: true, value: '_id', lable: 'depName' }" clearable placeholder="请选择部门" />
+        </el-form-item>
+      </el-form>
+      <template #footer>
+        <span class="dialog-footer">
+          <el-button @click="handleCancel">取 消</el-button>
+          <el-button type="primary" @click="handleSubmit">确 定</el-button>
+        </span>
+      </template>
+    </el-dialog>
   </div>
 </template>
 
 <script>
-import { reactive, ref, onMounted ,getCurrentInstance} from 'vue'
+import { reactive, ref, onMounted, getCurrentInstance } from 'vue'
 export default {
   name: 'User',
   setup() {
     // 获取代理对象
     const { proxy } = getCurrentInstance()
     const form = ref(null)
+    const dialogForm = ref(null)
     const user = reactive({
       userId: '',
       userName: '',
       state: 0,
     })
+    const showModel = ref(false)
     // 用户列表
     const userList = ref([])
     // 表格列配置
@@ -106,17 +151,51 @@ export default {
         prop: 'lastLoginTime'
       }
     ])
-
+    // 新增用户表单
+    const userForm = reactive({
+    })
     // 分页配置
     const pager = reactive({
-      pageNum:1,  // 当前页码
-      pageSize:10,  // 每页条数
-      total:10  // 总条数
+      pageNum: 1,  // 当前页码
+      pageSize: 10,  // 每页条数
+      total: 10  // 总条数
     })
+    // 新增用户表单验证规则
+    const rules = reactive({
+      userName: [
+        {
+          required: true,
+          message: '请输入用户名',
+          trigger: 'blur'
+        }
+      ],
+      userEmail: [
+        {
+          required: true,
+          message: '请输入用户邮箱',
+          trigger: 'blur'
+        }
+      ],
+      deptId: [
+        {
+          required: true,
+          message: '请选择部门',
+          trigger: 'blur'
+        }
+      ],
+      mobile: [
+        {
+          pattern: /^1[3-9]\d{9}$/,
+          message: '请输入正确的手机号',
+          trigger: 'blur'
+        }
+      ],
+    })
+    
     // 获取用户列表
     const getUserList = async () => {
-      let params = {...user, ...pager}
-      const {page, list} = await proxy.$api.userList(params)
+      let params = { ...user, ...pager }
+      const { page, list } = await proxy.$api.userList(params)
       userList.value = list
       pager.total = page.total
     }
@@ -136,7 +215,7 @@ export default {
     }
     // 用户单个删除
     const handleDelete = async (row) => {
-      const res = await proxy.$api.userDelete({userIds:[row.userId]})
+      const res = await proxy.$api.userDelete({ userIds: [row.userId] })
       if (res.nModified > 0) {
         proxy.$message.success('删除成功')
         getUserList()
@@ -151,7 +230,7 @@ export default {
         proxy.$message.warning('请选择要删除的用户')
         return
       }
-      const res = await proxy.$api.userDelete({userIds:checkedUsersIds.value})
+      const res = await proxy.$api.userDelete({ userIds: checkedUsersIds.value })
       if (res.nModified > 0) {
         proxy.$message.success('删除成功')
         getUserList()
@@ -167,25 +246,33 @@ export default {
       })
       checkedUsersIds.value = arr
     }
-
+    // 新增用户
+    const handleCreate = () => {
+      showModel.value = true
+    }
     // 组件挂载
     onMounted(() => {
       getUserList()
     })
 
     return {
+      showModel,
       form,
+      dialogForm,
       user,
       userList,
       columns,
       pager,
       checkedUsersIds,
+      userForm,
+      rules,
       handleQuery,
       handleReset,
       handleCurrentChange,
       handleDelete,
       handlePatchDelete,
-      handleSelectionChange
+      handleSelectionChange,
+      handleCreate
     }
   }
 }
